aad97f77e99e9dacec3ba66aacd0586e426e6a34
arjunprratap05 <108358230+arjunprratap05@users.noreply.github.com>
2025-07-23 23:13:17 +0530

    final commit

--- src/components/contact/contact-info/index.jsx
+++ src/components/contact/contact-info/index.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import "./style.scss";
+import "./style.scss"; 
 
 const ContactInfo = () => {
     return (
         <div className="contact-info-box">
-            <h4>Let's bridge the gap from screen to connection - looking forward to hearing from you ! </h4>
-            
+            <h4>Let's bridge the gap from screen to connection - looking forward to hearing from you!</h4>
+
             <div className="contact-option">
                 <FaPhoneAlt />
-                <span className="text"> +91 9820903458</span>
+                <span className="text">
+                    <a href="tel:+919820903458">+91 9820903458</a>
+                </span>
             </div>
             <div className="contact-option">
                 <MdEmail />
-                <span className="text"> arjun.pratap05@gmail.com</span>
+                <span className="text">
+                    <a href="mailto:arjun.pratap05@gmail.com">arjun.pratap05@gmail.com</a>
+                </span>
             </div>
         </div>
     );
