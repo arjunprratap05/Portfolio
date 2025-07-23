ff260e5779a80033bc5208727b86b6cd044c8bce
arjunprratap05 <108358230+arjunprratap05@users.noreply.github.com>
2025-07-23 22:48:52 +0530

    final commit

--- src/components/contact/contact-info/index.jsx
+++ src/components/contact/contact-info/index.jsx
@@ -6,7 +6,7 @@ import "./style.scss";
 const ContactInfo = () => {
     return (
         <div className="contact-info-box">
-            <h4>I would be happy to answer your questions </h4>
+            <h4>Let's bridge the gap from screen to connection - looking forward to hearing from you! </h4>
             <div className="contact-option">
                 <FaPhoneAlt />
                 <span className="text"> +91 9820903458</span>
