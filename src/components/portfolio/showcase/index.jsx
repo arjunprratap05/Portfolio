3b4fed3cdfd65a838b74bbd77d6d62a6381e86e1
ARJUN PRRATAP <arjunprratap05>
2025-12-31 18:08:47 +0530

    Final Commit

--- src/components/portfolio/showcase/index.jsx
+++ src/components/portfolio/showcase/index.jsx
@@ -12,21 +12,22 @@ const Showcase = ({ data, onProjectClick }) => {
         <div className="showcase-slider-wrapper">
             <Swiper
                 modules={[Navigation, Pagination, Autoplay]}
-                spaceBetween={20}
+                spaceBetween={15} 
                 slidesPerView={1} 
-                navigation
+                centeredSlides={true}
+                loop={true}
                 pagination={{ clickable: true }}
                 autoplay={{ delay: 4000, disableOnInteraction: false }}
                 breakpoints={{
-                
-                    1024: {
-                        slidesPerView: 3,
-                        spaceBetween: 30
+                    1024: { 
+                        slidesPerView: 3, 
+                        spaceBetween: 30, 
+                        centeredSlides: false 
                     },
-    
-                    768: {
-                        slidesPerView: 2,
-                        spaceBetween: 20
+                    768: { 
+                        slidesPerView: 2, 
+                        spaceBetween: 20, 
+                        centeredSlides: false 
                     }
                 }}
                 className="projects-swiper"
@@ -34,11 +35,11 @@ const Showcase = ({ data, onProjectClick }) => {
                 {data.map((project) => (
                     <SwiperSlide key={project.id}>
                         <div 
-                            className="project-card glass-effect"
+                            className="project-square-card"
                             onClick={() => onProjectClick(project)}
                         >
                             <div className="project-thumbnail-container">
-                                <img src={project.media.thumbnail} alt={project.name} className="project-img" />
+                                <img src={project.media.thumbnail} alt={project.name} />
                                 <div className="thumbnail-overlay">
                                      <div className="overlay-buttons">
                                         {project.codeUrl && ( 
@@ -56,12 +57,8 @@ const Showcase = ({ data, onProjectClick }) => {
                             </div>
                             <div className="project-info">
                                 <h4 className="project-title">{project.name}</h4>
-                                <p className="project-description">
-                                    {project.description.substring(0, 85)}... 
-                                    <span className="read-more"> Read More</span>
-                                </p>
                                 <div className="project-tags">
-                                    {project.tags.map((tag) => (
+                                    {project.tags.slice(0, 2).map((tag) => (
                                         <span key={tag} className="tag-item">#{tag.replace('-', '')}</span>
                                     ))}
                                 </div>
